ced526d7300f8580e8b9eb1ca029e86f93f1bd8f
vvlovsky <slvtrshkv@gmail.com>
2020-03-15 20:53:42 +0100

    finished plot style

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import base64
 from io import BytesIO
 
@@ -11,21 +12,25 @@ class MyFigure(Figure):
         """
         super().__init__(*args, **kwargs)
         self.text(0.5, 0.95, figtitle, ha='center')
-        self.patch.set_facecolor('#191919')
 
 
 def create_plot(data):
-    # Generate the figure **without using pyplot**.
-    plt.style.use('dark_background')
-    fig = plt.figure(FigureClass=MyFigure, figtitle='my title')
-    fig.patch.set_facecolor('#191919')
+    plt.rcParams['lines.linewidth'] = 3
+    plt.rcParams['axes.facecolor'] = '#191919'
+    plt.rcParams['savefig.facecolor'] = '#191919'
+    plt.rcParams['axes.edgecolor'] = 'white'
+    plt.rcParams['axes.labelcolor'] = 'white'
+    plt.rcParams['patch.edgecolor'] = 'white'
+    plt.rcParams['hatch.color'] = 'white'
+    plt.rcParams['xtick.color'] = 'white'
+    plt.rcParams['ytick.color'] = 'white'
+    plt.rcParams['axes.titlecolor'] = 'white'
+    fig = plt.figure(FigureClass=MyFigure, figtitle='')
     ax = fig.subplots()
 
-    ax.set_facecolor('#191919')
-
-    ax.plot(range(data[0]), data[1])
-    ax.plot(range(data[0]), data[2])
-    ax.plot(range(data[0]), data[3])
+    ax.plot(range(data[0]), data[1], c='#e15000')
+    ax.plot(range(data[0]), data[2], c='r')
+    ax.plot(range(data[0]), data[3], c='g')
 
     # Save it to a temporary buffer.
     buf = BytesIO()
