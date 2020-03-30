5a0ecf25305de4cbf9a9a6c8f0fb3fb4e043fc5d
vvlovsky <slvtrshkv@gmail.com>
2020-03-30 16:13:12 +0200

    README update

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # COVID-19_Outbreak_Simulation
 
 Computer simulation of COVID-19 outbreak, which is based on the deterministic
-differential equation model, complicated version of the SEIR-model. This model is
-calculated for each country separately when it becomes infected. A separate
+differential equation model, complicated version of the SEIR-model. 
+It is based on the article ["The effectiveness of quarantine and isolation determine the trend of the COVID-19 epidemics
+in the final phase of the current outbreak in China"](https://www.ijidonline.com/action/showPdf?pii=S1201-9712%2820%2930137-5) by Biao Tang, Fan Xia, Sanyi Tang, Nicola Luigi Bragazzi, Qian Li,
+Xiaodan Sun, Juhua Liang, Yanni Xiao, Jianhong Wu.\
+This model is calculated for each country separately when it becomes infected. A separate
 algorithm simulates border crossings using data from csv-files and identifies
-infected countries.
+infected countries.\
 This simulation has a web interface written with Django framework.
 
 ### Prerequisites
--- seir.py
+++ seir.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.integrate import odeint
 import math as math
-import matplotlib.pyplot as plt
 
 
 def deriv(y, t, N, c, beta, q, m, b, f, sigma, lamb, deltaI, gammaI, gammaH, alpha):
@@ -58,47 +57,4 @@ def seibqhr(c0, cb, r1, beta, q0, qm, r2, m, b, f, sigma, lamb, deltaI0, deltaIf
     ret = odeint(deriv, y0, t, args=(N, c, beta, q, m, b, f, sigma, lamb, deltaI, gammaI, gammaH, alpha))
     S, E, I, B, Q, H, R = ret.T
 
-    # print(S)
-    # for i in range(360):
-    # print(S[i] + "    " + E[i] + "     " + I[i] + "    " + R[i])
-
-    # Plot the data on three separate curves for S(t), I(t) and R(t)
-
     return S[-1], E[-1], I[-1], B[-1], Q[-1], H[-1], R[-1], c, q, deltaI
-
-# st, et, it, rt = 0, 0, 100, 0
-# n = 1000000000
-# S = [n - 100]
-# E = [0]
-# I = [100]
-# R = [0]
-# for i in range(250):
-#     n, st, et, it, rt = seir(n, 1 / 5.5, 0.4, 0.02, 0.00001, E[-1], I[-1], R[-1])
-#     S.append(st)
-#     E.append(et)
-#     I.append(it)
-#     R.append(rt)
-# print(S)
-#
-# fig = plt.figure(facecolor='w')
-# ax = fig.add_subplot(111, axisbelow=True)
-# tlin = np.linspace(0, 250, 251)
-# S = np.array(S)
-# E = np.array(E)
-# I = np.array(I)
-# R = np.array(R)
-# ax.plot(tlin, S / 1000000000, 'b', alpha=0.5, lw=2, label='Susceptible')
-# ax.plot(tlin, E / 1000000000, 'orange', alpha=0.5, lw=2, label='Susceptible')
-# ax.plot(tlin, I / 1000000000, 'r', alpha=0.5, lw=2, label='Infected')
-# ax.plot(tlin, R / 1000000000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
-# ax.set_xlabel('Time /days')
-# ax.set_ylabel('Number (1000s)')
-# ax.set_ylim(0, 1.2)
-# ax.yaxis.set_tick_params(length=0)
-# ax.xaxis.set_tick_params(length=0)
-# ax.grid(b=True, which='major', c='w', lw=2, ls='-')
-# legend = ax.legend()
-# legend.get_frame().set_alpha(0.5)
-# for spine in ('top', 'right', 'bottom', 'left'):
-#     ax.spines[spine].set_visible(False)
-# plt.show()
